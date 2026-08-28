8cc88c1dc40f88b800fce2523db0d34eb2762e2a
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-08-28 10:01:47 +0200

    Version 0.4.0

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
           jumphost = pkgs.rustPlatform.buildRustPackage {
             pname = "vpn-jumphost";
-            version = "0.3.0";
+            version = "0.4.0";
 
             src =
               let
